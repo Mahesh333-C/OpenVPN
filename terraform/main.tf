b0e10411658f2fe8d12f1ffccd2969f999bcdcf5
Mahesha-C <mahec@deloitte.com>
2023-09-07 17:08:10 +0530

    update-14

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,6 @@ resource "aws_vpc" "new_VPC" {
 terraform {
   backend "s3" {
     bucket         = "my-unique-bucket-name-333"
-    key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
