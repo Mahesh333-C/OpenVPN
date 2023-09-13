173eeaaafb37ab68788e193882fb51d959a3edf1
Mahesha-C <mahec@deloitte.com>
2023-09-13 11:27:53 +0530

    chnaged bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-unique-bucket-name-333"
+    bucket         = "my-unique-bucket-333"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
